d50c8c7efceee149ea423337a23906c3c3902b5c
gabriel.jospin <gabriel.jospin@contaswap.com>
2022-03-31 17:55:16 -0300

    fix entropy calc

--- classifier/DecisionTree.py
+++ classifier/DecisionTree.py
@@ -1,24 +1,26 @@
 import math
+import struct
 
 import numpy as np
 from abc import ABC
-
 import classifier as cls
 
 
 class DecisionTree(cls.Classifier, ABC):
 
+    
+
     def __init__(self):
         super().__init__()
 
     def train(self, x_train, y_train):
-        hent_tot = self.calc_entropy(y_train)
+        hent_tot = self.calc_amb_entropy(y_train)
         print(hent_tot)
-        hent = self.matrix_entropy(x_train)
-        print(hent)
-        hent_max = max(hent)
-        pos_max = hent.index(hent_max)
-        options = set(np.asarray(x_train.iloc[:, pos_max:pos_max+1]).ravel())
+        ent = self.matrix_entropy(x_train, y_train)
+        print(ent)
+        ent_max = max(ent)
+        pos_max = ent.index(ent_max)
+        options = set(np.asarray(x_train.iloc[:, pos_max:pos_max + 1]).flatten())
         print(options)
         pass
 
@@ -26,25 +28,45 @@ class DecisionTree(cls.Classifier, ABC):
         pass
 
     @staticmethod
-    def calc_entropy(list_in):
-        options = set(np.asarray(list_in).ravel())
-        list_in = np.array(list_in).T
-        x = len(options)
-        sum = 0
-        for i in options:
-            prob = list_in[list_in == i].shape[0] / len(list_in)
-            prob_in = list_in[list_in == i].sum() / list_in[list_in == i].shape[0]
-            if prob_in == 0:
-                sum += 0
-            else:
-                sum += prob * math.log2(prob_in)
-        return sum
+    def calc_binary_ent(prob):
+        if prob == 0:
+            return -(1 - prob) * math.log2(1 - prob)
+        if prob == 1:
+            return -(prob * math.log2(prob))
 
-    def matrix_entropy(self, matrix):
+        return -(prob * math.log2(prob) + (1 - prob) * math.log2(1 - prob))
+
+    def calc_amb_entropy(self, list_in):
+        list_in = np.asarray(list_in).T
+        prob = list_in[list_in == 1].shape[0] / len(list_in[0])
+        ent = self.calc_binary_ent(prob)
+        return ent
+
+    def calc_gain(self, list_in, list_out):
+
+
+        ent_geral = self.calc_amb_entropy(list_out)
+
+        list_in = np.asarray(list_in.T).T[0]
+        list_out = np.asarray(list_out).T[0]
+        tam = len(list_in)
+
+        options = set(list_in.flatten())
+        summary = 0
+
+        for op in options:
+            exemp_list_in = list_in[list_in == op]
+            exemp_list_out = list_out[list_in == op]
+            prob_glob = len(exemp_list_in) / tam
+            prob_loc = exemp_list_out.sum() / len(exemp_list_out)
+            summary += prob_glob * self.calc_binary_ent(prob_loc)
+
+        return ent_geral - summary
+
+    def matrix_entropy(self, matrix, out):
         matrix = np.matrix(matrix)
         hent = list()
         for collumn in matrix.T:
-            hent.append(self.calc_entropy(collumn))
+            hent.append(self.calc_gain(collumn, out))
 
         return hent
-
